5f397c028b24945504b7a9b37f25a150d187b1ff
sandeep-ipk <sandeep.ipk@gmail.com>
2018-01-20 15:51:59 +0530

    first commit

--- deepdream/inception5h.py
+++ deepdream/inception5h.py
@@ -1,34 +1,7 @@
 # this model is taken from internet, not mine
 
 
-########################################################################
-#
-# The Inception Model 5h for TensorFlow.
-#
-# This variant of the Inception model is easier to use for DeepDream
-# and other imaging techniques. This is because it allows the input
-# image to be any size, and the optimized images are also prettier.
-#
-# It is unclear which Inception model this implements because the
-# Google developers have (as usual) neglected to document it.
-# It is dubbed the 5h-model because that is the name of the zip-file,
-# but it is apparently simpler than the v.3 model.
-#
-# See the Python Notebook for Tutorial #14 for an example usage.
-#
-# Implemented in Python 3.5 with TensorFlow v0.11.0rc0
-#
-########################################################################
-#
-# This file is part of the TensorFlow Tutorials available at:
-#
-# https://github.com/Hvass-Labs/TensorFlow-Tutorials
-#
-# Published under the MIT License. See the file LICENSE for details.
-#
-# Copyright 2016 by Magnus Erik Hvass Pedersen
-#
-########################################################################
+
 
 import numpy as np
 import tensorflow as tf
